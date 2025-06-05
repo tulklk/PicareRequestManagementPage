e8d8bfa56c5b3dfc4f2e1e3208e291e69c11001d
QuangDao04 <nguyenquangdao1408@gmail.com>
2025-06-05 08:58:39 +0700

    hh

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,20 +8,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-  apiKey: "AIzaSyCEm5yiKbrO4jktVdea5RrSUeWAqMzhNHM",
-  authDomain: "pp-management-e8ea7.firebaseapp.com",
-  projectId: "pp-management-e8ea7",
-  storageBucket: "pp-management-e8ea7.firebasestorage.app",
-  messagingSenderId: "615869334261",
-  appId: "1:615869334261:web:8d571743ddccdbcd083bbc",
-  measurementId: "G-MYX2M72BW0"
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+
   };
 
 // Initialize Firebase
